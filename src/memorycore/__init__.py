33848a250fcb09b537921184a5c06b0c2f43d594
Carlosdanger96 <133168317+Carlosdanger96@users.noreply.github.com>
2026-07-16 15:20:30 -0600

    feat: export experience correction API

--- src/memorycore/__init__.py
+++ src/memorycore/__init__.py
@@ -1,7 +1,22 @@
 """Memorycore v0.1 public package."""
 
+from .experience import (
+    CorrectionOperation,
+    CorrectionOutcome,
+    ExperienceCorrection,
+    ExperienceCorrectionLedger,
+)
 from .memory_service import MemoryService
 from .models import Memory, MemoryStatus, MemoryType
 
-__all__ = ["Memory", "MemoryService", "MemoryStatus", "MemoryType"]
+__all__ = [
+    "CorrectionOperation",
+    "CorrectionOutcome",
+    "ExperienceCorrection",
+    "ExperienceCorrectionLedger",
+    "Memory",
+    "MemoryService",
+    "MemoryStatus",
+    "MemoryType",
+]
 __version__ = "0.1.0.dev0"
